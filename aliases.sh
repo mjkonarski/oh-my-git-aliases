811f501a95d1aa04c8126b5c630a72db339a6f47
Michal Konarski <michal.konarski@u2i.com>
2018-09-29 14:48:56 +0000

    Add current_branch function

--- aliases.sh
+++ aliases.sh
@@ -66,3 +66,9 @@ alias gstd='gsta drop'
 alias gcl='g clone'
 
 alias gr='g remote'
+
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
